736fe325b87005619e1f60cdf5f809d2a1b52b94
TO148757 <to148757@toll302228283.tls.fr.eu.airbus.corp>
2025-07-23 16:48:18 +0200

    More precise logging when matching 500, 4500 UDP flow

--- rawpacket.py
+++ rawpacket.py
@@ -32,6 +32,7 @@ class RawPacket:
             self.inner_l4 = udp.UDP(self.raw_data, self.inner_ipv4.header_end_byte, self.inner_ipv4.payload_length)
             #self.logger.debug(f"pre-processing inner_l4 : {self.inner_l4}")
             if self.inner_l4.dst_port in [500, 4500]:
+                self.logger.debug(raw_geneve_packet)
                 self.logger.debug(f"pre-processing geneve : {self.geneve}")
                 self.logger.debug(f"pre-precessing inner_ipv4 : {self.inner_ipv4}")
                 self.logger.debug(f"pre-processing inner_l4 : {self.inner_l4}")
@@ -84,6 +85,15 @@ class RawPacket:
                 self.logger.debug(f"post-processing geneve : {self.geneve}")
                 self.logger.debug(f"post-processing inner_ipv4 : {self.inner_ipv4}")
                 self.logger.debug(f"post-processing inner_l4 : {self.inner_l4}")
+                ret = b''.join([
+                self.outter_ipv4.repack(), 
+                self.outter_udp.repack(),
+                self.geneve.repack(),
+                self.inner_ipv4.repack(),
+                self.inner_l4.repack(),
+                self.raw_data[self.inner_l4.header_end_byte::]
+                ])
+                return ret
             return b''.join([
                 self.outter_ipv4.repack(), 
                 self.outter_udp.repack(),
