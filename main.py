ffd35549b82fcb8b12a167d6e6ddadff18302500
TO148757 <to148757@toll302228283.tls.fr.eu.airbus.corp>
2025-07-23 11:37:27 +0200

    Raw packet logging to hex

--- main.py
+++ main.py
@@ -230,7 +230,7 @@ def geneve_handler(geneve_packet, flow_tracker, udp_only=False):
     global logger
     try:
         rec_packet = RawPacket(logger, geneve_packet, flow_tracker, udp_only)
-        logger.debug(rec_packet)
+        logger.debug(geneve_packet.hex())
     except UnmatchedGenevePort:
         logger.debug("Ignoring packet received on non-Geneve port")
         return None
@@ -238,7 +238,7 @@ def geneve_handler(geneve_packet, flow_tracker, udp_only=False):
         logger.error(f"Unknown error while parsing new packet : {e}")
         return None
     resp = rec_packet.resp
-    logger.debug(resp)
+    logger.debug(resp.hex())
     return resp
 
 
